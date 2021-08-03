fdc76d1f3cbfc17b6de40a58061f0faae88828b2
Priscille Gwet <ngomakak@gmail.com>
2021-08-03 11:31:29 -0500

    Created a model called Favoris

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Favoris(models.Model):
+    name = models.CharField(max_length=200)
+    display_address = models.CharField(max_length=200)
+    display_phone =  models.CharField(max_length=200)
+    rating = models.DecimalField(max_digits=9, decimal_places=2)
+    image_url = models.URLField(max_length=200)
+  
+
+
+    def __str__(self):
+        return self.name
+    
