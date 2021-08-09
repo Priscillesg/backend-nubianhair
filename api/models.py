2032e617e599cf0e1ed266687c67626fa3d5134d
Priscille Gwet <ngomakak@gmail.com>
2021-08-09 15:01:06 -0500

    Added functions in api/views.py that will allow the user to display their favourites, save an item in their favourotes and remove an item from their favourites

--- api/models.py
+++ api/models.py
@@ -4,13 +4,14 @@ from django.db import models
 
 class Favoris(models.Model):
     name = models.CharField(max_length=200)
-    display_address = models.CharField(max_length=200)
-    display_phone =  models.CharField(max_length=200)
-    rating = models.DecimalField(max_digits=9, decimal_places=2)
     image_url = models.URLField(max_length=200)
+    business_id =  models.CharField(max_length=140, default='DEFAULT VALUE')
   
 
 
     def __str__(self):
         return self.name
     
+    # display_address = models.CharField(max_length=200)
+    # display_phone =  models.CharField(max_length=200)
+    # rating = models.DecimalField(max_digits=9, decimal_places=2)
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from api.models import Favoris
 from django.shortcuts import render
 from .serializers import FavorisSerializer, UserSerializer
-from rest_framework import viewsets
+from rest_framework import serializers, viewsets, status
 import requests
 from django.http import JsonResponse
 import os
@@ -10,7 +10,12 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
-from django.http import HttpResponseBadRequest
+from rest_framework.views import APIView 
+from django.http import HttpResponseBadRequest, HttpResponse
+from .models import Favoris
+from rest_framework.response import Response
+
+
 
 load_dotenv()
 
@@ -62,14 +67,63 @@ def business_detail(request, business_id):
 # save result once client add a business as favoris
 
 # ----------------------add favoris----------------------
+@api_view(["GET"])
+def favourites_list(request, slug):
+  
+    try:
+        favoris = Favoris.objects.get(slug=slug)
+    except Favoris.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = FavorisSerializer(favoris)
+        return Response(serializer.data)
 
-class FavorisViewSet(viewsets.ModelViewSet):
-    queryset = Favoris.objects.all()
-    serializer_class = FavorisSerializer
-    permission_classes = [IsAuthenticated]
-    authentication_classes = (TokenAuthentication,)
+# @api_view(["POST",])
+# def create_favourites(request):
+#         serializer = FavorisSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# ----------------------------
+@api_view(["GET"])
+def favouritesList(request):
+    favourites = Favoris.objects.all()
+    serializer = FavorisSerializer(favourites, many=True)
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def createList(request):
+    serializer = FavorisSerializer(data=request.data)
+    if serializer.is_valid():
+            serializer.save()
+    return Response(serializer.data)
+
+@api_view(["DELETE"])
+def deleteFavourite(request, pk):
+    favourite = Favoris.objects.get(id=pk)
+    favourite.delete()
+    # return Response(status=status.HTTP_204_NO_CONTENT)
+    return Response("Item successfully delete!")
+
+
+# class FavorisViewSet(viewsets.ModelViewSet):
+#     queryset = Favoris.objects.all()
+#     serializer_class = FavorisSerializer
+    # permission_classes = [IsAuthenticated]
+    # authentication_classes = (TokenAuthentication,)
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
+# class Locate_API(APIView):    # API view of the saved results in the database
+
+#     def get(self,request):
+#         results = Favoris.objects.all()
+#         serializer = FavorisSerializer(results, many = True)
+
+#         return Response(serializer.data)
